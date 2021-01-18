f6225ad2daea58c6b8de63c2ad5f457102cbbc55
Lucas Tigelaar <99061148@mydavinci.nl>
2021-01-18 14:34:12 +0100

    opdr 3 + 4 begin

--- opdracht3.js
+++ opdracht3.js
@@ -2,14 +2,11 @@ function vragen(){
 	var naam=prompt('Naam:');
 	var leeftijd=prompt('Leeftijd');
 }
-if(naam='stop'){
-	write();
-}if else(leeftijd='stop'){
-	write();
-}else(
-	vragen();
-)
-
 function write(){
 	document.write('Hallo '+naam+', je leeftijd is '+leeftijd);
+}
+if(naam='stop'){
+	write();
+}else{
+	vragen();
 }
--- opdracht4.js
+++ opdracht4.js
@@ -1,1 +1,10 @@
-alert("helllo");
+var number=prompt('Your number:');
+function tafels(){
+	for(var i=1;i<11;i++){
+		document.write(i+' x '+number+' = '+(i*number)+'<br>');
+	}
+}
+for(var i=0;i>=number;i++){
+	number=number+1
+	tafels();
+}
